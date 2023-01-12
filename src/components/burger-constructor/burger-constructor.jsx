d261d396290f63986ff4d7c55a66e7934829de86
heyxesha <petrovakseniait@mail.ru>
2023-01-12 19:45:52 +0300

    Убрала вызов модального окна по клику на элементы конструктара.

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -3,14 +3,12 @@ import { ConstructorElement, CurrencyIcon, Button, DragIcon } from '@ya.praktiku
 import IngridientsPropTypes from '../../utils/IngridientsPropTypes';
 import OrderDetails from '../order-details/order-details';
 import Modal from '../modal/modal';
-import IngredientDetails from '../ingredient-details/ingridient-details';
 import styles from './burger-constructor.module.css';
 
 const BurgerConstructor = ({ ingridients }) => {
     const [state, setState] = useState({
         modalVisibility: false,
-        modalChildren: null,
-        modalTitle: ''
+        modalChildren: null
     });
 
     // TODO: наверное, на следующих итерациях проекта сможем получать эти данные динамически.
@@ -31,33 +29,13 @@ const BurgerConstructor = ({ ingridients }) => {
             <OrderDetails orderId="034536" />
         );
         setState({
-            modalTitle: '',
             modalVisibility: true,
             modalChildren: orderDetails
         });
     };
 
-    const ingridientClick = (item) => {
-        const ingridientDetails = (
-            <IngredientDetails
-                name={ item.name }
-                image={ item.image_large }
-                calories={ item.calories }
-                proteins={ item.proteins }
-                fat={ item.fat }
-                carbohydrates={ item.carbohydrates }
-            />
-        );
-        setState({
-            modalTitle: 'Детали ингридиента',
-            modalVisibility: true,
-            modalChildren: ingridientDetails
-        });
-    };
-
     const close = () => {
         setState({
-            modalTitle: '',
             modalVisibility: false,
             modalChildren: null
         });
@@ -65,21 +43,18 @@ const BurgerConstructor = ({ ingridients }) => {
 
     return (
         <div className={ `${ styles.BurgerConstructor } pt-25 pl-4` }>
-            <div onClick={() => ingridientClick(bun)}>
-                <ConstructorElement
-                    extraClass={ `${ styles.Bun } ml-8 mr-4 ${ innerIngridients.length ? 'mb-4' : '' }` }
-                    type="top"
-                    isLocked={ true }
-                    text={ `${ bun.name } (верх)` }
-                    price={ bun.price }
-                    thumbnail={ bun.image }  />
-            </div>
+            <ConstructorElement
+                extraClass={ `${ styles.Bun } ml-8 mr-4 ${ innerIngridients.length ? 'mb-4' : '' }` }
+                type="top"
+                isLocked={ true }
+                text={ `${ bun.name } (верх)` }
+                price={ bun.price }
+                thumbnail={ bun.image }  />
             <div className={ styles.ScrollArea }>
                 {
                     innerIngridients.map((item, index) => (
                         <div
-                            key={ item._id } className={ `${ styles.DragItem } ${ index > 0 ? 'mt-4' : '' }` }
-                            onClick={ () => ingridientClick(item) }>
+                            key={ item._id } className={ `${ styles.DragItem } ${ index > 0 ? 'mt-4' : '' }` }>
                             <DragIcon />
                             <ConstructorElement
                                 extraClass={ `${styles.Ingridient} ml-2 mr-2` }
@@ -90,15 +65,13 @@ const BurgerConstructor = ({ ingridients }) => {
                     ))
                 }
             </div>
-            <div onClick={() => ingridientClick(bun)}>
-                <ConstructorElement
-                    extraClass={ `${ styles.Bun } ml-8 mr-4 mt-4` }
-                    type="bottom"
-                    isLocked={ true }
-                    text={ `${ bun.name } (низ)` }
-                    price={ bun.price }
-                    thumbnail={ bun.image } />
-            </div>
+            <ConstructorElement
+                extraClass={ `${ styles.Bun } ml-8 mr-4 mt-4` }
+                type="bottom"
+                isLocked={ true }
+                text={ `${ bun.name } (низ)` }
+                price={ bun.price }
+                thumbnail={ bun.image } />
             <div className={ `${ styles.TotalBlock } mt-10` }>
                 <div className={ `${ styles.Sum } mr-10` }>
                     <div className="text text_type_digits-medium mr-1">
@@ -116,7 +89,6 @@ const BurgerConstructor = ({ ingridients }) => {
             </div>
             { state.modalVisibility &&
                 <Modal
-                    title={ state.modalTitle }
                     children={ state.modalChildren }
                     onClose={ close } /> 
             }
